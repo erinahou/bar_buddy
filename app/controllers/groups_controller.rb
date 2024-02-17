20fbbe73c9770540762d05fa2a4ac6fefa87fdcd
Airton McGready <airtonmcgready@live.com.au>
2024-02-17 15:28:36 +1100

    Did routes, controllers, views

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,26 @@
 class GroupsController < ApplicationController
+  def new
+  end
+
+  def index
+  end
+
+  def yes
+  end
+
+  def no
+  end
+
+  def confirm
+  end
+
+  def verdict
+  end
+
+  def destroy_member
+  end
+
+  def add_member
+  end
+
 end
