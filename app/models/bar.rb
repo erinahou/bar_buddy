660481124ce577802d32cc884e5e037c941b1441
Erina Ho <erina.hou@gmail.com>
2024-02-17 11:12:39 +1100

    Added validations for each model

--- app/models/bar.rb
+++ app/models/bar.rb
@@ -1,2 +1,4 @@
 class Bar < ApplicationRecord
+  has_many :bar_characteristics
+  has_many :characteristics, through: :bar_characteristics
 end
--- app/models/characteristic.rb
+++ app/models/characteristic.rb
@@ -1,2 +1,4 @@
 class Characteristic < ApplicationRecord
+  has_many :bar_characteristics
+  has_many :bars, through: :bar_characteristics
 end
