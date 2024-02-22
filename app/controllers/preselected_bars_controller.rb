b8651730c41eb58a18498d533a8e276a8f4d5c2d
Erina Ho <erina.hou@gmail.com>
2024-02-22 19:45:46 +1100

    defined preselected bar controller

--- app/controllers/preselected_bars_controller.rb
+++ app/controllers/preselected_bars_controller.rb
@@ -1,5 +1,13 @@
 class PreselectedBarsController < ApplicationController
+  before_action :set_group
+
   def index
-    @preselected_bars = PreselectedBar.all;
+    @preselected_bars = @group.preselected_bars
+  end
+
+  private
+
+  def set_group
+    @group = Group.find(params[:group_id])
   end
 end
