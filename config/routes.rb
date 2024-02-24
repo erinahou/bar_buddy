66052659822ebec25ab013bb294d9af3e23928f3
Erina Ho <erina.hou@gmail.com>
2024-02-24 15:15:59 +1100

    Cleaned up route to keep only necessities

--- config/routes.rb
+++ config/routes.rb
@@ -4,30 +4,23 @@ Rails.application.routes.draw do
   get '/dashboard', to: 'pages#dashboard'
 
   # Bars
-  resources :bars, only: [:show] do
-    resources :votes, only: [:index, :new, :create]
-    resources :bar_characteristics, only: [:index]
-  end
+  resources :bars, only: [:show]
 
-  # Characteristics
-  resources :characteristics, only: [:index]
+  # removed votes
+  # remove bar_characteristics
+  # removed characteristics
 
   # Groups
-  resources :groups do
+  resources :groups, only: [:new, :create, :edit] do # Specified only necessary actions
     member do
-      get 'confirm'
       get 'verdict'
-      delete 'destroy_member'
-      post 'add_member'
+      get 'confirmation'
     end
-    resources :members, only: [:index, :show]
-    resources :votes, only: [:index, :new, :create]
-    resources :preselected_bars, only: [:index,:new, :create]
+    resources :members, only: [:new, :create]
+    resources :preselected_bars, only: [:index, :new, :create] do
+      resources :votes, only: [:create] # Nesting votes under preselected bars to get this path /groups/:group_id/preselected_bars/:preselected_bar_id/votes
+    end
   end
 
-  # Users
-  resources :users
-
-  # Votes
-  resources :votes, only: [:index, :show]
+  # removed resources :users because this is already done through devise.
 end
