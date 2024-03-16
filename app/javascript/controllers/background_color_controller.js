d49042085a7877ca111e21fdbcdc4347fe46732f
Erina Ho <erina.hou@gmail.com>
2024-03-16 14:56:31 +1100

    Styling fixes, moving gradient background

--- app/javascript/controllers/background_color_controller.js
+++ app/javascript/controllers/background_color_controller.js
@@ -7,8 +7,7 @@ export default class extends Controller {
 
     if (currentRoute.includes('verdict')) {
       this.element.style.backgroundColor = "#DAFF35";
-    } else if (currentRoute.includes('preselected_bars') && !currentRoute.includes('new')){
-      this.element.style.backgroundColor = "rgb(166, 77, 255)";
+
     }
   }
 }
