fa524fdd0f91d3a166fab36134cd012c6c316ba6
Erina Ho <erina.hou@gmail.com>
2024-03-23 10:48:52 +1100

    Fixed gradient bkg issue verdict

--- app/javascript/controllers/background_color_controller.js
+++ app/javascript/controllers/background_color_controller.js
@@ -5,12 +5,9 @@ export default class extends Controller {
   connect() {
     const currentRoute = this.element.dataset.currentRoute;
 
-    // if (currentRoute.includes('new')) {
-    //   this.element.classList.remove("gradient-body");
-    // }
-
     // if (currentRoute.includes('verdict')) {
-    //   this.element.style.backgroundColor = "#DAFF35";
+    //   this.element.classList.remove("gradient-body");
+    //   this.element.classList.add("gradient-body-victory");
     // }
   }
 }
