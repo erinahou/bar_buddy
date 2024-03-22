6cb0fd830e998ffbd5692d7e3a2958d4521e38a0
rohan martin <rohan1215@hotmail.com>
2024-03-22 16:51:30 +1100

    fixed confetti and added js to handle vote submission

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -3,20 +3,27 @@ class VotesController < ApplicationController
     @group = Group.find(params[:group_id])
     preselected_bars = @group.preselected_bars
 
+    Rails.logger.info "Received parameters: #{params.inspect}"
+
+    vote_params = JSON.parse(request.body.read)['votes']
+
     preselected_bars.each do |preselected_bar|
+      vote_value = vote_params[preselected_bar.id.to_s]
+      Rails.logger.info "Vote value for preselected_bar #{preselected_bar.id}: #{vote_value}"
+
       vote = Vote.new(
         user_id: current_user.id,
         preselected_bar_id: preselected_bar.id,
-        vote: params["vote_#{preselected_bar.id}"] == "true"
+        vote: ActiveRecord::Type::Boolean.new.cast(vote_value)
       )
 
       unless vote.save
         flash[:alert] = "Failed to record votes."
-        all_votes_saved = false
         redirect_to @group
         return
       end
     end
+
     redirect_to confirmation_group_path(@group)
   end
 
--- app/javascript/controllers/swiper_controller.js
+++ app/javascript/controllers/swiper_controller.js
@@ -11,7 +11,21 @@ export default class extends Controller {
     });
   }
 
+  getPreselectedBarId(slideIndex) {
+    const slideElement = this.swiper.slides[slideIndex];
+    return slideElement.dataset.preselectedBarId;
+  }
+
   swipeNext(event) {
+    const voteValue = event.currentTarget.getAttribute('data-swiper-vote-value');
+    console.log('Vote value:', voteValue);
+
+    const currentSlide = this.swiper.activeIndex;
+    const preselectedBarId = this.getPreselectedBarId(currentSlide);
+
+
+    this.element.dataset[`vote_${preselectedBarId}`] = voteValue;
+
     if (this.isLastSlide()) {
       this.submitForm();
     } else {
@@ -24,7 +38,36 @@ export default class extends Controller {
   }
 
   submitForm() {
-    const form = this.element; // Access the form element
-    form.submit(); // Submit the form
+    const form = this.element;
+
+
+    const voteData = {};
+    this.swiper.slides.forEach((slide, index) => {
+      const preselectedBarId = this.getPreselectedBarId(index);
+      const voteValue = form.dataset[`vote_${preselectedBarId}`];
+      voteData[preselectedBarId] = voteValue;
+    });
+
+    console.log('Vote data:', voteData);
+
+
+    fetch(form.action, {
+      method: form.method,
+      headers: {
+        'Content-Type': 'application/json',
+        'X-CSRF-Token': document.querySelector('meta[name="csrf-token"]').content
+      },
+      body: JSON.stringify({ votes: voteData })
+    })
+    .then(response => {
+      if (response.ok) {
+        window.location.href = response.url;
+      } else {
+        console.error('Failed to record votes');
+      }
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
   }
 }
